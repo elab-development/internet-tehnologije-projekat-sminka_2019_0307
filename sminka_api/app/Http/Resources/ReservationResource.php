82654c7ad491054a539fee8cd08273429050db14
lazar <pajovic.lazar036@gmail.com>
2024-05-15 20:10:36 +0200

    Resursi

---
+++ sminka_api/app/Http/Resources/ReservationResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Service;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReservationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $service = Service::find($this->service_id);
+        $user = User::find($this->user_id);
+
+        return [
+            'id' => $this->id,
+            'service' => new ServiceResource($service),
+            'reservation_date' => $this->reservation_date,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'message' => $this->message,
+            'user' => new UserResource($user)
+        ];
+
+    }
+}
---
+++ sminka_api/app/Http/Resources/ServiceResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Type;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ServiceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $type = Type::find($this->type_id);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'price' => $this->price,
+            'description' => $this->description,
+            'type' => new TypeResource($type)
+        ];
+    }
+}
