e07f8dee65ff46885af9b2b2ec6f0c2e1050fac0
Sanja Petkovic <spsanjapetkovic33@gmail.com>
2024-05-18 11:26:33 +0200

    Routes

--- sminka_api/routes/api.php
+++ sminka_api/routes/api.php
@@ -17,3 +17,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', '\App\Http\Controllers\LoginController@login');
+Route::post('/register', '\App\Http\Controllers\LoginController@register');
+
+Route::apiResource('/types', '\App\Http\Controllers\TypeController')->only(['index', 'show']);
+Route::apiResource('/services', '\App\Http\Controllers\ServiceController')->only(['index', 'show']);
+Route::apiResource('/reservations', '\App\Http\Controllers\ReservationController')->only(['index', 'show']);
+
+Route::get('/find', '\App\Http\Controllers\ServiceController@findByName');
+Route::get('/grouped-data', '\App\Http\Controllers\ServiceController@groupByType');
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::apiResource('/services', '\App\Http\Controllers\ServiceController')->only(['store', 'update', 'destroy']);
+    Route::apiResource('/reservations', '\App\Http\Controllers\ReservationController')->only(['store', 'update', 'destroy']);
+    Route::apiResource('/types', '\App\Http\Controllers\TypeController')->only(['store', 'update', 'destroy']);
+    Route::get('/paginate', '\App\Http\Controllers\ReservationController@paginate');
+
+    Route::post('/logout', '\App\Http\Controllers\LoginController@logout');
+});
